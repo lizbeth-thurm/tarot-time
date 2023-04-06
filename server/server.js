059d368e71b8796ca3c681a02535802b24875443
slthurman01 <slthurman01@gmail.com>
2023-04-06 11:48:20 -0500

    Adds routes, formats server and conn

--- server/server.js
+++ server/server.js
@@ -15,10 +15,9 @@ app.use(require("./routes/record"));
 const dbo = require("./db/conn");
 
 app.listen(port, () => {
-    // Perform a database connection when server starts
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
-
-    });
-    console.log(`Server is running on port: ${port}`);
+  // Perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
 });
